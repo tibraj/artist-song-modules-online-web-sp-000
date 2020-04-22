f692c7a96aae4481eda6db589225a085860e1a20
Taulant Ibraj <tibraj94@gmail.com>
2020-04-22 16:09:13 +0000

    Automatically backed up by Learn

--- lib/concerns/findable.rb
+++ lib/concerns/findable.rb
@@ -1,7 +1,7 @@
 module Findable
-
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
+  module ClassMethods
+    def self.find_by_name(name)
+      @@artists.detect{|a| a.name == name}
+    end
   end
-
 end
